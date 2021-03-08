527854efeded8e52456db88735609dcbee4d5425
ashrafwkhalil <ashrafdude@gmail.com>
2021-03-08 11:21:26 -0500

    work completed

--- src/server/main.go
+++ src/server/main.go
@@ -15,22 +15,59 @@ type counters struct {
 	click int
 }
 
-var (
-	c = counters{}
+//this is a map of 4 counters for each content category. Views and clicks will be stored here until uploaded to the store
+var counterMap = map[string]counters{
+	"sports":        counters{},
+	"entertainment": counters{},
+	"business":      counters{},
+	"education":     counters{},
+}
 
-	content = []string{"sports", "entertainment", "business", "education"}
-)
+var content = []string{"sports", "entertainment", "business", "education"}
+
+//this is the counter store
+var counterStore = map[string]counters{}
+
+//this is just a variable to lock to modify counters with
+var c = counters{}
+
+//global variable to monitor status requests within 10 second window
+var statusRequests = 0
+
+//the last request time made to the status page, initialized as 20 seconds ago, so first request will automatically reinitialize this variable
+//(becuase isallowed() reinitializes this variable if a request is made and the last request was more than 10 seconds ago)
+var lastRefresh = time.Now().Unix() - 20
+
+//function that is executed after every time counters are dumped into the store.
+func makeCounters() {
+	counterMap = map[string]counters{
+		"sports":        counters{},
+		"entertainment": counters{},
+		"business":      counters{},
+		"education":     counters{},
+	}
+}
 
 func welcomeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Welcome to EQ Works 😎")
+
 }
 
 func viewHandler(w http.ResponseWriter, r *http.Request) {
 	data := content[rand.Intn(len(content))]
 
+	//pointer to counter of interest
+	c = counterMap[data]
+
+	fmt.Fprintln(w, "You are viewing")
+	fmt.Fprintln(w, data)
 	c.Lock()
+	//increments counter here
 	c.view++
+
 	c.Unlock()
+	//places new value of counter back into map
+	counterMap[data] = c
 
 	err := processRequest(r)
 	if err != nil {
@@ -42,6 +79,7 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 	// simulate random click call
 	if rand.Intn(100) < 50 {
 		processClick(data)
+
 	}
 }
 
@@ -51,10 +89,12 @@ func processRequest(r *http.Request) error {
 }
 
 func processClick(data string) error {
+	//same logic as view, put counter in pointer, increment, put back.
+	var c = counterMap[data]
 	c.Lock()
 	c.click++
 	c.Unlock()
-
+	counterMap[data] = c
 	return nil
 }
 
@@ -66,14 +106,39 @@ func statsHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func isAllowed() bool {
-	return true
+	// time windows are 10 seconds long, you are allowed 10 requests per window. every 10 seconds, a new window is opened. If your last request was more than 10 seconds ago, a new window starts
+	// the goal is to make it so that spamming requests 10 times in a row blocks the request.
+	if lastRefresh < (time.Now().Unix() - 10) {
+		lastRefresh = time.Now().Unix()
+		statusRequests = 1
+		return true
+	}
+	if statusRequests < 10 {
+		statusRequests++
+		return true
+	}
+	return false
 }
 
 func uploadCounters() error {
+	//here, every 5 seconds, all contents of the counter map are moved to the counter store
+	//the counter store will store all data about views and clicks in each category for each 5 second interval.
+	for true {
+		time.Sleep(5000 * time.Millisecond)
+		var i = 0
+		for i < 4 {
+			counterStore[content[i]+time.Now().String()] = counterMap[content[i]]
+			i++
+		}
+		makeCounters()
+
+	}
+
 	return nil
 }
 
 func main() {
+	go uploadCounters()
 	http.HandleFunc("/", welcomeHandler)
 	http.HandleFunc("/view/", viewHandler)
 	http.HandleFunc("/stats/", statsHandler)
